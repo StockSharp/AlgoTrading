45c14cf8406fdadc748a3e8c21591a20c09b367d
mika@stocksharp.com <mika@stocksharp.com>
2025-07-04 21:18:27 +0300

    build fix.

--- Tests/StrategyTests.cs
+++ Tests/StrategyTests.cs
@@ -1,8 +1,12 @@
+namespace StockSharp.Tests;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Ecng.Logging;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,72 +15,66 @@ using StockSharp.Algo.Strategies;
 using StockSharp.Algo.Testing;
 using StockSharp.BusinessEntities;
 using StockSharp.Configuration;
-using StockSharp.Logging;
-using StockSharp.Messages;
-using StockSharp.Samples.HistoryData;
 
-namespace StockSharp.Tests
+[TestClass]
+public class StrategyTests
 {
-	[TestClass]
-	public class StrategyTests
+	public static IEnumerable<object[]> StrategyTypes => typeof(StockSharp.Samples.Strategies.MaCrossoverStrategy)
+		.Assembly
+		.GetTypes()
+		.Where(t => typeof(Strategy).IsAssignableFrom(t) && !t.IsAbstract && t.Namespace == "StockSharp.Samples.Strategies")
+		.Select(t => new object[] { t });
+
+	[DataTestMethod]
+	[DynamicData(nameof(StrategyTypes))]
+	public async Task RunStrategy(Type strategyType)
 	{
-		public static IEnumerable<object[]> StrategyTypes => typeof(StockSharp.Samples.Strategies.MaCrossoverStrategy)
-			.Assembly
-			.GetTypes()
-			.Where(t => typeof(Strategy).IsAssignableFrom(t) && !t.IsAbstract && t.Namespace == "StockSharp.Samples.Strategies")
-			.Select(t => new object[] { t });
+		var strategy = (Strategy)Activator.CreateInstance(strategyType)!;
+		await Run(strategy);
+	}
 
-		[DataTestMethod]
-		[DynamicData(nameof(StrategyTypes))]
-		public async Task RunStrategy(Type strategyType)
+	private static async Task Run(Strategy strategy)
+	{
+		var logManager = new LogManager();
+		logManager.Listeners.Add(new ConsoleLogListener());
+
+		var token = CancellationToken.None;
+
+		var secId = Paths.HistoryDefaultSecurity;
+		var security = new Security { Id = secId };
+
+		var storageRegistry = new StorageRegistry { DefaultDrive = new LocalMarketDataDrive(Paths.HistoryDataPath) };
+
+		var startTime = Paths.HistoryBeginDate;
+		var stopTime = Paths.HistoryEndDate;
+
+		var pf = Portfolio.CreateSimulator();
+		pf.CurrentValue = 1000000m;
+
+		var connector = new HistoryEmulationConnector([security], [pf], storageRegistry)
 		{
-			var strategy = (Strategy)Activator.CreateInstance(strategyType)!;
-			await Run(strategy);
-		}
-
-		private static async Task Run(Strategy strategy)
-		{
-			var logManager = new LogManager();
-			logManager.Listeners.Add(new ConsoleLogListener());
-
-			var token = CancellationToken.None;
-
-			var secId = Paths.HistoryDefaultSecurity;
-			var security = new Security { Id = secId };
-
-			var storageRegistry = new StorageRegistry { DefaultDrive = new LocalMarketDataDrive(Paths.HistoryDataPath) };
-
-			var startTime = Paths.HistoryBeginDate;
-			var stopTime = Paths.HistoryEndDate;
-
-			var pf = Portfolio.CreateSimulator();
-			pf.CurrentValue = 1000000m;
-
-			var connector = new HistoryEmulationConnector(new[] { security }, new[] { pf }, storageRegistry)
+			HistoryMessageAdapter =
 			{
-				HistoryMessageAdapter =
-				{
-					StartDate = startTime,
-					StopDate = stopTime,
-				}
-			};
+				StartDate = startTime,
+				StopDate = stopTime,
+			}
+		};
 
-			strategy.Portfolio = pf;
-			strategy.Security = security;
-			strategy.Connector = connector;
-			strategy.Volume = 1;
+		strategy.Portfolio = pf;
+		strategy.Security = security;
+		strategy.Connector = connector;
+		strategy.Volume = 1;
 
-			logManager.Sources.Add(connector);
-			logManager.Sources.Add(strategy);
+		logManager.Sources.Add(connector);
+		logManager.Sources.Add(strategy);
 
-			await connector.ConnectAsync(token);
+		await connector.ConnectAsync(token);
 
-			var task = strategy.ExecAsync(null, token);
-			connector.Start();
-			await task.AsTask();
+		var task = strategy.ExecAsync(null, token);
+		connector.Start();
+		await task.AsTask();
 
-			Assert.IsTrue(strategy.Orders.Count() > 10, $"{strategy.GetType().Name} placed {strategy.Orders.Count()} orders");
-			Assert.IsTrue(strategy.MyTrades.Count() > 5, $"{strategy.GetType().Name} executed {strategy.MyTrades.Count()} trades");
-		}
+		Assert.IsTrue(strategy.Orders.Count() > 10, $"{strategy.GetType().Name} placed {strategy.Orders.Count()} orders");
+		Assert.IsTrue(strategy.MyTrades.Count() > 5, $"{strategy.GetType().Name} executed {strategy.MyTrades.Count()} trades");
 	}
 }
