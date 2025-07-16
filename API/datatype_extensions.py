e17c0dd178cd8a17bddfb875ae63188a100d61c1
mika@stocksharp.com <mika@stocksharp.com>
2025-07-16 18:46:36 +0300

    Code cleanup.

--- API/datatype_extensions.py
+++
@@ -1,9 +0,0 @@
-import clr
-clr.AddReference("StockSharp.Messages")
-clr.AddReference("StockSharp.Algo")
-from System import TimeSpan
-from StockSharp.Messages import DataType
-
-def tf(minutes):
-    """Return TimeFrame candle type for the given number of minutes."""
-    return DataType.TimeFrame(TimeSpan.FromMinutes(minutes))
