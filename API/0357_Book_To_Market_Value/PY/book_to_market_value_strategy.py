aa27e2e53ff557f30f19b75c9c7c17f9df6296c0
Mikhail <mikasoukhov@users.noreply.github.com>
2025-08-06 00:20:48 +0300

    Add Python Book-to-Market Value strategy with documentation

---
+++ API/0357_Book_To_Market_Value/PY/book_to_market_value_strategy.py
@@ -0,0 +1,80 @@
+import clr
+
+clr.AddReference("StockSharp.Messages")
+clr.AddReference("StockSharp.Algo")
+
+from System import TimeSpan
+from StockSharp.Messages import DataType
+from StockSharp.Algo.Strategies import Strategy
+from datatype_extensions import *
+
+class book_to_market_value_strategy(Strategy):
+    """Book-to-market value strategy placeholder."""
+
+    def __init__(self):
+        super(book_to_market_value_strategy, self).__init__()
+
+        self._universe = self.Param("Universe", []) \
+            .SetDisplay("Universe", "Securities to process", "General")
+
+        self._min_trade_usd = self.Param("MinTradeUsd", 200.0) \
+            .SetGreaterThanZero() \
+            .SetDisplay("Min Trade USD", "Minimum trade value in USD", "General")
+
+        self._candle_type = self.Param("CandleType", TimeSpan.FromDays(1).TimeFrame()) \
+            .SetDisplay("Candle Type", "Type of candles to use", "General")
+
+    # region Properties
+    @property
+    def Universe(self):
+        """Securities universe to analyze."""
+        return self._universe.Value
+
+    @Universe.setter
+    def Universe(self, value):
+        self._universe.Value = value
+
+    @property
+    def MinTradeUsd(self):
+        """Minimum trade value in USD."""
+        return self._min_trade_usd.Value
+
+    @MinTradeUsd.setter
+    def MinTradeUsd(self, value):
+        self._min_trade_usd.Value = value
+
+    @property
+    def CandleType(self):
+        """The type of candles to use for strategy calculation."""
+        return self._candle_type.Value
+
+    @CandleType.setter
+    def CandleType(self, value):
+        self._candle_type.Value = value
+    # endregion
+
+    def GetWorkingSecurities(self):
+        return [(s, self.CandleType) for s in self.Universe]
+
+    def OnReseted(self):
+        super(book_to_market_value_strategy, self).OnReseted()
+
+    def OnStarted(self, time):
+        super(book_to_market_value_strategy, self).OnStarted(time)
+
+        if self.Universe is None or len(self.Universe) == 0:
+            raise Exception("Universe is empty.")
+
+        trigger = self.Universe[0]
+
+        self.SubscribeCandles(self.CandleType, True, trigger) \
+            .Bind(lambda candle: self.OnDay(candle.OpenTime.Date)) \
+            .Start()
+
+    def OnDay(self, date):
+        """Placeholder for factor logic executed each day."""
+        pass
+
+    def CreateClone(self):
+        """Creates a new instance of the strategy."""
+        return book_to_market_value_strategy()
