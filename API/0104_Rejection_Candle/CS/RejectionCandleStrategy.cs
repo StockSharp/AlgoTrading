4f74f2b0513aac8a8f121bfd70374f03a362d817
Mikhail <mikasoukhov@users.noreply.github.com>
2025-09-27 19:06:08 +0300

    Add standard using directives to strategies 0051-0200 (#4084)

--- API/0104_Rejection_Candle/CS/RejectionCandleStrategy.cs
+++ API/0104_Rejection_Candle/CS/RejectionCandleStrategy.cs
@@ -1,12 +1,18 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
+
 using Ecng.Common;
-using StockSharp.Algo;
-using StockSharp.Algo.Candles;
+using Ecng.Collections;
+using Ecng.Serialization;
+
 using StockSharp.Algo.Indicators;
 using StockSharp.Algo.Strategies;
 using StockSharp.BusinessEntities;
 using StockSharp.Messages;
+
+using StockSharp.Algo;
+using StockSharp.Algo.Candles;
 
 namespace StockSharp.Samples.Strategies;
 
