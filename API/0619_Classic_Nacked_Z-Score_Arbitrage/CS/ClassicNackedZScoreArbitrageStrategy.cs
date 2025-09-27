df8b5e80bf71c701755748ea32fbde6ab1b04c7a
Mikhail <mikasoukhov@users.noreply.github.com>
2025-09-27 19:04:13 +0300

    Add common using directives to strategies 0501-1000

--- API/0619_Classic_Nacked_Z-Score_Arbitrage/CS/ClassicNackedZScoreArbitrageStrategy.cs
+++ API/0619_Classic_Nacked_Z-Score_Arbitrage/CS/ClassicNackedZScoreArbitrageStrategy.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
-using Ecng.ComponentModel;
+using Ecng.Common;
+using Ecng.Collections;
+using Ecng.Serialization;
 
 using StockSharp.Algo.Indicators;
 using StockSharp.Algo.Strategies;
 using StockSharp.BusinessEntities;
 using StockSharp.Messages;
+
+using Ecng.ComponentModel;
 
 namespace StockSharp.Samples.Strategies;
 
--- API/0815_Funamental_and_financials/CS/FunamentalAndFinancialsStrategy.cs
+++ API/0815_Funamental_and_financials/CS/FunamentalAndFinancialsStrategy.cs
@@ -1,6 +1,15 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
+using Ecng.Common;
+using Ecng.Collections;
+using Ecng.Serialization;
+
+using StockSharp.Algo.Indicators;
 using StockSharp.Algo.Strategies;
+using StockSharp.BusinessEntities;
+using StockSharp.Messages;
 
 namespace StockSharp.Samples.Strategies;
 
