9296b98bb39ebbee92aa75afe9bf5fb7448a05c5
mika <mika@stocksharp.com>
2026-03-08 07:51:09 +0300

    Small changes.

--- API/3839_Flat_Trend/CS/FlatTrendStrategy.cs
+++ API/3839_Flat_Trend/CS/FlatTrendStrategy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 using StockSharp.Algo.Indicators;
 using StockSharp.Algo.Strategies;
@@ -35,9 +36,12 @@ public class FlatTrendStrategy : Strategy
 		_atrPeriod = Param(nameof(AtrPeriod), 14)
 			.SetDisplay("ATR Period", "ATR volatility period", "Indicators");
 
-		_candleType = Param(nameof(CandleType), TimeSpan.FromMinutes(5).TimeFrame())
+		_candleType = Param(nameof(CandleType), TimeSpan.FromHours(4).TimeFrame())
 			.SetDisplay("Candle Type", "Candle timeframe", "General");
 	}
+
+	public override IEnumerable<(Security sec, DataType dt)> GetWorkingSecurities() => [(Security, CandleType)];
+	protected override void OnReseted() { base.OnReseted(); _prevAtr = 0m; _prevClose = 0m; _prevEma = 0m; _hasPrev = false; }
 
 	protected override void OnStarted2(DateTime time)
 	{
